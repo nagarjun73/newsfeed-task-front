1d88a5f86ef0a7044ef7b6f3df3c8c089b17e447
Nagarjun <111555377+nagarjun73@users.noreply.github.com>
2024-01-05 23:40:04 +0530

    added navbar and newsfeed functionality

--- src/components/Feed/NewsFeed.js
+++ src/components/Feed/NewsFeed.js
@@ -1,8 +1,31 @@
-const NewsFeed = (props) => {
-  return (
-    <div>
+import { Box } from "@mui/material"
+import { useEffect, useState } from "react"
+import axios from "axios"
+import NewsCard from "./NewsCard"
 
-    </div>
+const NewsFeed = (props) => {
+  const [option, setOption] = useState("recentStories")
+  const [feeds, setFeeds] = useState([])
+  console.log(feeds);
+
+  useEffect(() => {
+    (async () => {
+      const getFeed = await axios.get(`http://localhost:3073/feeds/${option}`, {
+        headers: {
+          Authorization: localStorage.getItem('token')
+        }
+      })
+      setFeeds(getFeed.data);
+    })()
+  }, [])
+
+
+  return (
+    <Box paddingTop="20vh">
+      {feeds.map((feed) => {
+        return <NewsCard key={feed._id} feed={feed} />
+      })}
+    </Box>
   )
 }
 
