b4609ba9f52b1a356afbe61a8e8e96097e328edf
Nagarjun <111555377+nagarjun73@users.noreply.github.com>
2024-01-11 08:21:52 +0530

    added comments

--- src/helpers/getCategoryData.js
+++ src/helpers/getCategoryData.js
@@ -1,7 +1,5 @@
 import axios from "../config/axiosConfig"
 
-
-
 const getCategoryData = async (userDispatch) => {
   const getCategory = await axios.get('http://localhost:3073/categories/all', {
     headers: {
